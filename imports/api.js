5b3b5559ec468c1aa6850dde8b7a58b820e45447
stuartish <stuartrballard@gmail.com>
2018-03-08 21:36:07 -0600

    cleanup

--- imports/api.js
+++ imports/api.js
@@ -3,13 +3,11 @@ import { HTTP } from 'meteor/http';
 export const Coins = new Mongo.Collection('coins');
 
 if (Meteor.isServer) {   
-	//TODO: figure out a more elegant, idiomatic way to do this
 	function pollTicker(){
 		var ticker = HTTP.get('https://api.coinmarketcap.com/v1/ticker/?limit=10').data;
 		for (var i = 0; i < ticker.length; i++) {
 			Coins.upsert(ticker[i].id,
 				{
-				// Modifier
 				 $set: {
 					name: ticker[i].name,
 					price_usd: ticker[i].price_usd
@@ -18,5 +16,5 @@ if (Meteor.isServer) {
 		}
 	}
 	pollTicker();
-	Meteor.setInterval(pollTicker, 300000);
+	Meteor.setInterval(pollTicker, 10000);
 }
